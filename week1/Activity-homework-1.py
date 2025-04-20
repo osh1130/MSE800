b16c5ad980b7a73632663a1e60f2e5340d65f357
osh1130 <vivizhao401@gmail.com>
2025-04-21 11:40:25 +1200

    week1 homework

---
+++ week1/Activity-homework-1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.arange(1, 11)
+print("Array:", arr)
+print("Shape:", arr.shape)
+print("Data Type:", arr.dtype)
+
+arr_doubled = arr * 2
+print("Array multiplied by 2:", arr_doubled)
